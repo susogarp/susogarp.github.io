6aeef2c2b2cfe67d724afad7f42f8c3cbf3bb889
SusoGarcia <susogarciape@gmail.com>
2020-01-07 02:01:24 -0400

    Add portfolio

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+const page = document.querySelector('body');
+const nav = document.querySelector('.nav__menu');
+const navToggle = document.querySelector('.nav__menu-btn');
+const navClose = document.querySelector('.nav__menu-close');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.add('nav--mobile');
+  navClose.style.display = 'block';
+  page.style.backgroundColor = 'rgba(0,0,0,.4)';
+});
+
+navClose.addEventListener('click', () => {
+  nav.classList.remove('nav--mobile');
+  navClose.style.display = 'none';
+  page.style.backgroundColor = 'rgba(255,255,255,1)';
+});
